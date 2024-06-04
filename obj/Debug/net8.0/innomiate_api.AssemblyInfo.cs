3cda0289e60d89026bc79489e35b6b60d0d240b9
DeginToWebsite <zineb.zineb.zouzou@gmail.com>
2024-06-04 20:36:23 +0100

    nn

--- obj/Debug/net8.0/innomiate_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/innomiate_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("innomiate_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c23dd67cc4addeb5f4516edcf878d243409418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e244d603c97c50dd5ad7c613394c22f644992657")]
 [assembly: System.Reflection.AssemblyProductAttribute("innomiate_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("innomiate_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
